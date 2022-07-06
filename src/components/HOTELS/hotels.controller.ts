a84754a44d6a75b0ee8fdab6a7cf8f230a81d1e6
GreatEki <ekeneidiagbor@gmail.com>
2022-07-06 16:19:28 +0100

    feat: added delete hotel feature

--- src/components/HOTELS/hotels.controller.ts
+++ src/components/HOTELS/hotels.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Hotels } from "../../models";
+import NotFoundError from "../../errors/NotFoundError";
 
 export const createHotel = async (req: Request, res: Response) => {
   try {
@@ -12,6 +13,112 @@ export const createHotel = async (req: Request, res: Response) => {
       statusCode: 201,
       message: "New Hotel Created",
       data: newHotel,
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      status: err.status,
+      statusCode: err.statusCode,
+      message: err.message,
+    });
+  }
+};
+
+export const updateHotel = async (req: Request, res: Response) => {
+  try {
+    const { hotelId } = req.params;
+
+    const updateHotelData = await Hotels.findByIdAndUpdate(
+      hotelId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+
+    return res.json({
+      success: true,
+      status: "OK",
+      statusCode: 200,
+      message: "Hotel Information updated",
+      data: updateHotelData,
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      status: err.status,
+      statusCode: err.statusCode,
+      message: err.message,
+    });
+  }
+};
+
+export const getHotelById = async (req: Request, res: Response) => {
+  try {
+    const { hotelId } = req.params;
+    const theHotel = await Hotels.findById({ _id: hotelId });
+
+    if (!theHotel) {
+      throw new NotFoundError("Hotel not found");
+    }
+
+    if (theHotel) {
+      return res.json({
+        success: true,
+        status: "OK",
+        statusCode: 200,
+        message: "Hotel retrieved successfully",
+        data: theHotel,
+      });
+    }
+  } catch (err) {
+    return res.json({
+      success: false,
+      status: err.status,
+      statusCode: err.statusCode,
+      message: err.message,
+    });
+  }
+};
+
+export const getAllHotels = async (req: Request, res: Response) => {
+  try {
+    const hotels = await Hotels.find();
+
+    return res.json({
+      success: true,
+      status: "OK",
+      statusCode: 200,
+      count: hotels.length,
+      message: "Hotels retrieved successfully",
+      data: hotels,
+    });
+  } catch (err) {
+    return res.json({
+      success: false,
+      status: err.status,
+      statusCode: err.statusCode,
+      message: err.message,
+    });
+  }
+};
+
+export const deleteHotel = async (req: Request, res: Response) => {
+  try {
+    const { hotelId } = req.params;
+
+    const delHotel = await Hotels.findByIdAndDelete({ _id: hotelId });
+
+    if (!delHotel) {
+      throw new NotFoundError("No hotel with id found");
+    }
+
+    return res.json({
+      success: true,
+      status: "OK",
+      statusCode: 200,
+      message: "Hotel Deleted",
+      data: delHotel,
     });
   } catch (err) {
     return res.json({
--- src/components/HOTELS/hotels.router.ts
+++ src/components/HOTELS/hotels.router.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { createHotel } from "./hotels.controller";
+import {
+  createHotel,
+  deleteHotel,
+  getAllHotels,
+  getHotelById,
+  updateHotel,
+} from "./hotels.controller";
 
 const router = Router();
 
-router.route("/").post(createHotel);
+router.route("/").post(createHotel).get(getAllHotels);
+router
+  .route("/:hotelId")
+  .get(getHotelById)
+  .put(updateHotel)
+  .delete(deleteHotel);
 
 export default router;
